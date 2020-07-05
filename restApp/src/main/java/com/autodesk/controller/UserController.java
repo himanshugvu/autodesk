94682b6b612202f47c9f728c6a264ac0d252e836
dbshack2hire3 <hack2hire03@hack2hire.net>
2020-07-05 14:15:14 +0800

    Deployment

--- restApp/src/main/java/com/autodesk/controller/UserController.java
+++ restApp/src/main/java/com/autodesk/controller/UserController.java
@@ -32,24 +32,4 @@ public class UserController {
         }
     }
 
-	/*
-	 * @RequestMapping(value="/admin/adminHome", method = RequestMethod.GET) public
-	 * String home(){ String String = new String(); Authentication auth =
-	 * SecurityContextHolder.getContext().getAuthentication(); User user =
-	 * userService.findUserByEmail(auth.getName()); String.addObject("userName",
-	 * "Welcome " + user.getName() + " " + user.getLastName() + " (" +
-	 * user.getEmail() + ")"); String.addObject(
-	 * "adminMessage","This Page is available to Users with Admin Role");
-	 * String.setViewName("admin/adminHome"); return String; }
-	 * 
-	 * @RequestMapping(value="/user/userHome", method = RequestMethod.GET) public
-	 * String user(){ String String = new String(); Authentication auth =
-	 * SecurityContextHolder.getContext().getAuthentication(); User user =
-	 * userService.findUserByEmail(auth.getName()); String.addObject("userName",
-	 * "Welcome " + user.getName() + " " + user.getLastName() + " (" +
-	 * user.getEmail() + ")"); String.addObject(
-	 * "userMessage","This Page is available to Users with User Role");
-	 * String.setViewName("user/userHome"); return String; }
-	 */
-
 }
