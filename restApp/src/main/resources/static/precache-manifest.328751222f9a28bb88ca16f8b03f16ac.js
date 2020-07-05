cf9b390a6b93018979b0dc38bb4cb22ee47437de
dbshack2hire3 <hack2hire03@hack2hire.net>
2020-07-06 04:27:07 +0800

    ds

---
+++ restApp/src/main/resources/static/precache-manifest.328751222f9a28bb88ca16f8b03f16ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ce0ca254b22bc943a638e8c69c5cd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2c1193ceec13139dc69",
+    "url": "/static/css/main.d8064672.chunk.css"
+  },
+  {
+    "revision": "dce74b3081f2a1159293",
+    "url": "/static/js/2.217dcf5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.217dcf5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c1193ceec13139dc69",
+    "url": "/static/js/main.7b4359aa.chunk.js"
+  },
+  {
+    "revision": "a18433267d9e13d7dacf",
+    "url": "/static/js/runtime-main.cb801cac.js"
+  }
+]);
